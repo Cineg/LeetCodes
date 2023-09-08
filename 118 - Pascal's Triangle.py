ded969126591f9a7c8d6279f67c832a742a51b72
cineg <cineg7@gmail.com>
2023-09-08 17:57:15 +0200

    solved 118

---
+++ 118 - Pascal's Triangle.py
@@ -0,0 +1,32 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        final_list: list = []
+        for row in range(numRows):
+            row_list = []
+
+            if row == 1:
+                row_list = [1,1]
+                
+            if row == 0:
+                row_list = [1]
+
+            if row > 1:
+                row_list = [1]
+                i = 1
+                while i < len(final_list[row-1]):
+                    value = final_list[row-1][i] + final_list[row-1][i-1]
+                    row_list.append(value)
+                    i += + 1
+
+                row_list.append(1)
+
+            final_list.append(row_list)
+            
+        return final_list
+    
+def main():
+    sol = Solution()
+    sol.generate(5)
+
+if __name__ == "__main__":
+    main()
