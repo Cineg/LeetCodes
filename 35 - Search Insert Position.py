88e3e90654c7fec01a7626dcb94d7c609316777c
cineg <cineg7@gmail.com>
2023-09-14 18:16:47 +0200

    solved 35

---
+++ 35 - Search Insert Position.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        for index, value in enumerate(nums):
+            if value >= target:
+                return index
+    
+        return len(nums)
+
